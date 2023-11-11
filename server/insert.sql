9fd585a4721181b0d3dbb042ac80ba2280d52358
CiobanuPaul <ciobanuioanpaul43@gmail.com>
2023-11-12 01:44:12 +0200

    Added problems

---
+++ server/insert.sql
@@ -0,0 +1,20 @@
+insert into problem(ptr_file, nume)
+values("id_10", "pietricele")
+
+insert into problem(ptr_file, nume)
+values("id_11", "biom")
+
+insert into problem(ptr_file, nume)
+values("id_12", "comun")
+
+insert into problem(ptr_file, nume)
+values("id_13", "xidartros")
+
+insert into problem(ptr_file, nume)
+values("id_14", "bolovani")
+
+insert into problem(ptr_file, nume)
+values("id_15", "schema")
+
+insert into problem(ptr_file, nume)
+values("id_16", "colibri")
