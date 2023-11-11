60f98b45468204c850f95213056eb6e9eef3cd0c
Buciu Theodor Marian <theodor.buciu@proton.me>
2023-11-11 19:44:42 +0200

    Revert "moved front to server "hope it works""
    This reverts commit 687418015aaed4d87965309db3bff1e82dee9807.

--- server/dist/front/src/Problems.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function Pb({ name }) {
-    return <p>{name}</p>;
-}
-function Problems() {
-    return (<Pb name='problem'/>);
-}
-exports.default = Problems;
