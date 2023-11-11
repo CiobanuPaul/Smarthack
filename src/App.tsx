76d3aac17c6c70e5636e0414690feed957a86851
CiobanuPaul <ciobanuioanpaul43@gmail.com>
2023-11-11 15:09:12 +0200

    Adaugat o problema

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  let items = ["unu", "doi", "trei"]
   return (
     <>
       <div>
@@ -16,18 +16,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>My first React experiment</h1>
+      {items.map(item => (
+        <h3>Se va afisa: {item}</h3>
+      ))}
     </>
   )
 }
