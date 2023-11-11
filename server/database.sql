c3fc1fd5949996d807bb9193aa0a3217416f7d01
Stefan-Mocanu <stefanmo.sm@gmail.com>
2023-11-11 15:19:44 +0200

    DATABASE update

--- server/database.sql
+++ server/database.sql
@@ -1,57 +1,61 @@
 use smarthack;
 
 create table user(
-    id_user number(5) constraint usr_pk primary key,
+    id_user DECIMAL(5),
     nume varchar(25),
-    premune varchar(25),
+    prenune varchar(25),
     email varchar(50),
-    data_inscriere date
+    data_inscriere date,
+    constraint usr_pk primary key(id_user)
 );
 
 create table problem(
-    id_pb number(5) constraint pb_pk primary key,
-    ptr_file varchar(25)
+    id_pb DECIMAL(5),
+    ptr_file varchar(25),
+    constraint pb_pk primary key(id_pb)
 );
 
 create table categorie(
-    id_cat number(5),
-    nume varchar(25)
+    id_cat DECIMAL(5),
+    nume varchar(25),
+     constraint cat_pk primary key(id_cat)
 );
 
 create table AIReqs(
-    id_pb number(5),
-    id_req number(5),
+    id_pb DECIMAL(5),
+    id_req DECIMAL(5),
     nume varchar(25),
     constraint AIR_pk primary key(id_pb,id_req),
-    constraint AIR_pb_fk foreign key(id_pb) references problem
+    constraint AIR_pb_fk foreign key(id_pb) references problem(id_pb)
 );
 
 create table Pb_Cat(
-    id_pb number(5),
-    id_cat number(5),
-    difficulty number(3),
+    id_pb DECIMAL(5),
+    id_cat DECIMAL(5),
+    difficulty DECIMAL(3),
     constraint PC_pk primary key(id_pb,id_cat),
-    constraint PC_pb_fk foreign key(id_pb) references problem,
-    constraint PC_cat_fk foreign key(id_cat) references categorie
+    constraint PC_pb_fk foreign key(id_pb) references problem(id_pb),
+    constraint PC_cat_fk foreign key(id_cat) references categorie(id_cat)
 );
 
 create table rulare(
-    id_rulare number(5) constraint rul_pk primary key,
-    id_user number(5),
-    id_pb number(5),
+    id_rulare DECIMAL(5),
+    id_user DECIMAL(5),
+    id_pb DECIMAL(5),
     time date,
-    nota_rulare number(3),
-    solution_ptr varchar(25); 
-    constraint rul_usr_fk foreign key(id_user) references user,
-    constraint rul_pb_fk foreign key(id_pb) references problem
+    nota_rulare DECIMAL(3),
+    solution_ptr varchar(25), 
+    constraint rul_usr_fk foreign key(id_user) references user(id_user),
+    constraint rul_pb_fk foreign key(id_pb) references problem(id_pb),
+    constraint rul_pk primary key(id_rulare)
 );
 
 create table noteAI(
-    id_rulare number(5),
-    id_pb number(5),
-    id_req number(5),
-    nota number(3),
-    constraint NAI_rul_fk foreign key(id_rul) references rulare,
-    constraint NAI_pbreq_fk foreign key(id_pb,id_req) references AIReqs,
+    id_rulare DECIMAL(5),
+    id_pb DECIMAL(5),
+    id_req DECIMAL(5),
+    nota DECIMAL(3),
+    constraint NAI_rul_fk foreign key(id_rul) references rulare(id_rul),
+    constraint NAI_pbreq_fk foreign key(id_pb,id_req) references AIReqs(id_pb,id_req),
     constraint NAI_pk primary key(id_rulare,id_pb,pd_req)
 );
