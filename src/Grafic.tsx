18fcd3ef7316d8c16045c2944fd232c8c56c6602
CiobanuPaul <ciobanuioanpaul43@gmail.com>
2023-11-12 11:27:00 +0200

    hbbh

---
+++ src/Grafic.tsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from 'react'
+
+
+export default function Tabel(){
+    const [date, setDate] = useState<Array<any>>([]);
+    useEffect(()=>{
+        
+    }, [])
+
+}
