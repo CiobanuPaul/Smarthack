167ff0d5d3951f40b30d735e94374c32cf9c9ea7
CiobanuPaul <ciobanuioanpaul43@gmail.com>
2023-11-12 10:28:15 +0200

    Incercat descpb

--- src/Problems.tsx
+++ src/Problems.tsx
@@ -6,8 +6,8 @@ import ReactMarkdown from "react-markdown"
 
 export function Problems() {
     const [problems, setProblems] = useState<any>([])
-    const [selected, setSelected] = useState<object>({})
-    if(selected == {}){
+    const [selected, setSelected] = useState<number>(-1)
+    if(selected == -1){
       useEffect(() => {
         fetch('http://localhost:3001/problems')
         .then(res => res.json())
@@ -17,7 +17,7 @@ export function Problems() {
       }, [])
       //console.log(problems)
       let arr:any=[]
-      problems.map((p:any) => arr.push(<p onClick={()=>setSelected(p)}>{p.id_pb}. {p.nume}</p>))
+      problems.map((p:any) => arr.push(<p onClick={()=>setSelected(p.id_pb)}>{p.id_pb}. {p.nume}</p>))
       console.log(arr)
     return (
         <ul>
@@ -28,15 +28,16 @@ export function Problems() {
     )  
     } 
     else {
-      fetch('http://localhost:3001/pbanume', {
-        method: 'POST',
-        body: JSON.stringify({
-          selected
-        }), headers: {
+      fetch(`http://localhost:3001/descpb?selected=${selected}`, {
+        method: 'GET',
+         headers: {
           'Content-Type':'application/json'
         }
       }).then((res:Response)=>{
-        return <ReactMarkdown>res</ReactMarkdown>;
+        return res.json();
+      }).then((data)=>{
+        console.log(data);
+        return <ReactMarkdown>data.toString()</ReactMarkdown>
       })
         
     } 
