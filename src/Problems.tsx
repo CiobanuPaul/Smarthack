8def33e2aad2efb2a99cd6d7450bf4c922cac509
CiobanuPaul <ciobanuioanpaul43@gmail.com>
2023-11-12 11:03:10 +0200

    good descpb

--- src/Problems.tsx
+++ src/Problems.tsx
@@ -1,45 +1,61 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
+import Markdown from 'react-markdown'
 import ReactMarkdown from "react-markdown"
 
 
 
 
-export function Problems() {
-    const [problems, setProblems] = useState<any>([])
-    const [selected, setSelected] = useState<number>(-1)
-    if(selected == -1){
-      useEffect(() => {
-        fetch('http://localhost:3001/problems')
+export default function Problems() {
+  const [problems, setProblems] = useState<any>([])
+  const [selected, setSelected] = useState<number>(-1)
+  const [description, setDescription] = useState<string>('')
+  if (selected == -1) {
+    useEffect(() => {
+      fetch('http://localhost:3001/problems')
         .then(res => res.json())
         .then(data => {
-            setProblems(data)
+          setProblems(data)
         })
-      }, [])
-      //console.log(problems)
-      let arr:any=[]
-      problems.map((p:any) => arr.push(<p onClick={()=>setSelected(p.id_pb)}>{p.id_pb}. {p.nume}</p>))
-      console.log(arr)
-    return (
-        <ul>
-         {
-            arr
-         }   
-        </ul>
-    )  
-    } 
-    else {
-      fetch(`http://localhost:3001/descpb?selected=${selected}`, {
-        method: 'GET',
-         headers: {
-          'Content-Type':'application/json'
-        }
-      }).then((res:Response)=>{
-        return res.json();
-      }).then((data)=>{
-        console.log(data);
-        return <ReactMarkdown>data.toString()</ReactMarkdown>
-      })
-        
-    } 
+    }, [])
+    //console.log(problems)
+    let arr: any = []
     
+    problems.map((p: any) => arr.push(<p onClick={() => setSelected(p.id_pb)}>{p.id_pb}. {p.nume}</p>))
+    console.log(arr)
+    return (
+      <ul>
+        {
+          arr
+        }
+      </ul>
+    )
+  }
+  else {
+    // await fetch(`http://localhost:3001/descpb?selected=${selected}`, {
+    //   method: 'GET',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   }
+    // }).then((res: Response) => {
+    //   if (res.body == null) { throw "why"; }
+    //   return res.text()
+    // }).then((data) => {
+    //   console.log(data);
+    //   return <Markdown>data</Markdown>;
+    // })
+    useEffect(() => {
+      fetch(`http://localhost:3001/descpb?selected=${selected}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }).then(res => res.text())
+        .then(data => {
+          setDescription(data)
+        })
+    }, [])
+    console.log(description)
+    return <Markdown>{description}</Markdown>;
+  }
+
 }
